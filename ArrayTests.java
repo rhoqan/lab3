48d4c7e7cb9283245d7e3987fbc3b2522809ee09
rhoqan <114369881+rhoqan@users.noreply.github.com>
2022-10-12 14:34:47 -0700

    fixed reverse

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,17 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReversedWithMore() {
+    int[] input1 = {1, 9, 24};
+    assertArrayEquals(new int[]{24, 9, 1}, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReversedInPlaceWithMore() {
+    int[] input1 = {1, 9, 24, 36};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{36, 24, 9, 1}, input1);
+  }
 }
