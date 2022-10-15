f8ff46e863b434286b8e81dfb97b7b52ad87a3bf
rhoqan <114369881+rhoqan@users.noreply.github.com>
2022-10-14 18:48:09 -0700

    fixed linked list append

--- LinkedListTests.java
+++ LinkedListTests.java
@@ -2,5 +2,15 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class LinkedListTests {
-    
+    @Test
+	public void testAppend() {
+		LinkedList llist1 = new LinkedList();
+		llist1.append(2);
+		llist1.append(3);
+		llist1.append(5);
+		llist1.append(7);
+		String expectedString = "2 3 5 7 ";
+        assertEquals(expectedString, llist1.toString());
+	}
+
 }
